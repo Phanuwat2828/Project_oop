7fc9c8d68a8e4580c8f2c9d5fef13d3614660b21
Markbackgoeshome <unisamx2@gmail.com>
2024-07-21 21:16:29 +0700

    update_21/7

--- PM2.5/page3/frametwo.java
+++ PM2.5/page3/frametwo.java
@@ -6,19 +6,20 @@ import java.awt.Graphics;
 public class frametwo {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Rectangle Frame");
-        frame.setSize(1000, 300); 
+        frame.setSize(1980, 1080); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-
+        
         JPanel panel = new JPanel() {
+            /*----- method create rectangle set size and position --------*/
             protected void paintComponent(Graphics g) {
-
+        
                 g.setFont(new Font("Tahoma", Font.BOLD, 15)); 
-
+                  /*------- สร้าง object and กำหนด position and size of React  ----------*/
                 ColorRectangle redRect = new ColorRectangle(20, 15, 110, 100, Color.RED, "จำนวนผู้ป่วย", "มากกว่า30%");
                 ColorRectangle orangeRect = new ColorRectangle(255, 15, 110, 100, new Color(255, 125, 0), "จำนวนผู้ป่วย", "มากกว่า30%");
                 ColorRectangle yellowRect = new ColorRectangle(485, 15, 110, 100, Color.YELLOW, "จำนวนผู้ป่วย", "มากกว่า30%");
                 ColorRectangle greenRect = new ColorRectangle(725, 15, 110, 100, Color.GREEN, "จำนวนผู้ป่วย", "มากกว่า30%");
-
+                
                 redRect.draw(g);
                 orangeRect.draw(g);
                 yellowRect.draw(g);
