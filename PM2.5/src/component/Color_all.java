e15524f57688494ee1e88fd51547fbe6c960a247
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-07-22 15:22:46 +0700

    update layout

--- PM2.5/src/component/Color_all.java
+++ PM2.5/src/component/Color_all.java
@@ -6,6 +6,7 @@ public class Color_all {
        public Color cl_bg = new Color(195,179,238);
        public Color cl_bg_red = new Color(255,0,0);
        public Color cl_bg_white = new Color(255,255,255);
+       public Color cl_bg_gray = new Color(217,217,217);
 }
 
 
