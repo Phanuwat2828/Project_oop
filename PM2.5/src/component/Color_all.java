424b3b127ff0df5d4d3c920ccb620add99a561ce
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-07-25 03:07:52 +0700

    update

--- PM2.5/src/component/Color_all.java
+++ PM2.5/src/component/Color_all.java
@@ -4,14 +4,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Color_all {
-<<<<<<< HEAD
-       public Color cl_bg = new Color(195,179,238);
-       public Color cl_bg_red = new Color(255,0,0);
-       public Color cl_bg_white = new Color(255,255,255);
-       public Color cl_bg_gray = new Color(217,217,217);
-       public Color cl_bg_bt = new Color(215,156,229);
-       
-=======
        public Color cl_bg = new Color(195, 179, 238);
        public Color cl_bg_red = new Color(255, 0, 0);
        public static Color cl_bg_white = new Color(255, 255, 255);
@@ -25,5 +17,4 @@ public class Color_all {
        public Color cl_li_or = new Color(255, 200, 150);
        public Color cl_li_green = new Color(200, 255, 200);
        public Color cl_li_yellow = new Color(255, 255, 200);
->>>>>>> Phanuwat
 }
